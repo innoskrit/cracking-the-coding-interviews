45e2c135bc22bcaad4611d5684322076e990a976
prernasharma <prernakaruna24@gmail.com>
2023-09-05 13:25:19 +0530

    added SearchInsertPosition

---
+++ master-binary-search/CPP/SearchInsertPosition.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+class Solution {
+public:
+    int searchInsert(std::vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+};
